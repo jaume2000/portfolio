9a6212070dd54b30406ca29e94a58cdc77b8134d
jaume2000 <jaumeig95@gmail.com>
2023-08-02 01:34:52 +0200

    Testing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     i18n: {
-        locales: ["en","es","fr"],
+        locales: ["en","es"],
         defaultLocale: "en",
         localeDetection: true
     }
