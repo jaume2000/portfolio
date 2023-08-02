453e9c177fd70d7cc661fbc63fd7e87de3bae169
jaume2000 <jaumeig95@gmail.com>
2023-08-02 02:24:05 +0200

    Erased i18n

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ["default","en","es","fr"],
-        defaultLocale: "default",
-        localeDetection: false
-    },
-    trailingSlash: true,
 }
 
 module.exports = nextConfig
