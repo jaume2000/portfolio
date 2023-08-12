4af286a0965e40dfbca2fc59b614fa82f501cf52
jaume2000 <jaumeig95@gmail.com>
2023-08-12 03:07:20 +0200

    added some text for the workd I will put

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -13,14 +13,21 @@ const Works = ({lang}: {lang:string})=>{
                 {dict.title}
             </h1>
             <div className='works_grid'>
-                <div className='W1 grid_element'>
-                    
+                <div className='W1 grid_element centered'>
+                    <p>{"Portfolio project"}</p>
                 </div>
-                <div className='W2 grid_element'/>
-                <div className='W3 grid_element'/>
-                <div className='W4 grid_element'/>
-                <div className='W5 grid_element'/>
-                <div className='W6 grid_element'/>
+                <div className='W2 grid_element centered'>
+                    <p>{"Perceptron C#"}</p>
+                </div>
+                <div className='W3 grid_element centered'>
+                    <p>{"Logic Gate Simulator"}</p>
+                </div>
+                <div className='W4 grid_element centered'>
+                    <p>{"Fine-Tuned Stable Diffusion"}</p>
+                </div>
+                <div className='W5 grid_element centered'>
+                    <p>{"Neurocatching Web"}</p>
+                </div>
             </div>
         </div>
     )
