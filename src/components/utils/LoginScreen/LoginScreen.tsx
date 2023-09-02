87b84e4180731a41099f171afcb10b6f046a5f06
jaume2000 <jaumeig95@gmail.com>
2023-09-02 02:13:02 +0200

    added little circle to the animation

--- src/components/utils/LoginScreen/LoginScreen.tsx
+++ src/components/utils/LoginScreen/LoginScreen.tsx
@@ -3,10 +3,14 @@ import './style.css'
 
 export default function LoginScreen() {
   return (
-    <div className="loading_screen">
-      <div id="loading_screen_part1"></div>
-      <div id="loading_screen_part2"></div>
-      <div id="loading_screen_part3"></div>
+    <div className='loading_screen'>
+      <div className="loading_animation">
+        <div id="loading_screen_part1"></div>
+        <div id="loading_screen_part2"></div>
+        <div id="loading_screen_part3"></div>
+      </div>
+      <div id="loading_screen_circle"></div>
     </div>
+
   )
 }
