5fc53aff890d8dfa7cdb335ff18c39a6c2586579
jaume2000 <jaumeig95@gmail.com>
2024-08-11 02:13:44 +0200

    actualizado el leaderboard

--- src/app/[lang]/mycrospacerank/route.ts
+++ src/app/[lang]/mycrospacerank/route.ts
@@ -13,8 +13,41 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
 
     console.log(data);
 
+    let processed_data = []
+
+    const contacts: { [email: string]: string } = {
+      "juliams2707@gmail.com": "Júlia Molina Sánchez",
+      "andrgope05@gmail.com": "Andrés Gómez Peiró",
+      "mathiasbernb@gmail.com": "Mathias Bern",
+      "mariajosevp2005@gmail.com": "María José Vila Pastor",
+      "eliasdlpmu@gmail.com": "Elias de la Peña Muñoz",
+      "mariasc453@gmail.com": "Maria Salcedo Ciscar",
+      "sergioariasmass@gmail.com": "Sergio Arias Galiano",
+      "leyre.roca326@gmail.com": "Leyre Roca",
+      "yoliversal2002@gmail.com": "Yolanda Vera Salmerón",
+      "hernandezsorianocarlos@gmail.com": "Carlos Hernández Soriano",
+      "alicianavarronupv@gmail.com": "Alicia Navarro Navarro",
+      "lara.uniupv@gmail.com": "Lara Barona Pinilla",
+      "lotfiomar2501@gmail.com": "Omar Loft-Allah",
+      "jcarpinterobasalo@gmail.com": "Judith Carpintero Basalo",
+      "iratxegb133@gmail.com": "Iratxe García Bascuñan",
+      "claudiaherrerop@gmail.com": "Claudia (literalmente mi madre)",
+      "david.degois.suarez@gmail.com": "David de Gois Suarez",
+      "jaumeig95@gmail.com": "Jaume Ivars (el vago que no taggea)",
+    };
+    
+
+
+    for (let i = 0; i < data.length; i++) {
+      let element = data[i];
+      if (element[0] in contacts) {
+        element[0] = contacts[element[0]];
+      }
+      processed_data.push([element[0], element[1]]);
+    }
+
     // Retornar la respuesta como JSON
-    return NextResponse.json(data);
+    return NextResponse.json(processed_data);
   } catch (error) {
     console.error('Error fetching data:', error);
 
